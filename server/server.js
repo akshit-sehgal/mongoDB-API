b2bfe433e1be83db5578c2365169b168fa0b0260
Akshit Sehgal <akshitsehgal95@gmail.com>
2017-12-02 14:36:48 +0530

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // C:\Program Files\MongoDB\Server\3.4\bin>mongod.exe --dbpath E:/Node-js/MongoDB-data
+const _=require('lodash');
 var express=require('express');
 var bodyParser=require('body-parser');
 var {mongoose}=require('./db/mongoose');
@@ -51,6 +52,25 @@ Todo.findByIdAndRemove(id).then((todo)=>{
 }).catch((e)=>{
     res.status(400).send();
 })
+});
+app.patch('/todos/:id',(req,res)=>{
+    var id=req.params.id;
+    var body=_.pick(req.body,['text','completed']);
+    if(!ObjectID.isValid(id))
+    return res.send().status(404);
+    if(_.isBoolean(body.completed)&&body.completed)
+    body.completedAt=new Date().getTime();
+    else{
+        body.completed=false;
+        body.completedAt=null;
+    }
+    Todo.findByIdAndUpdate(id, {$set:body},{new:true}).then((todo)=>{
+        if(!todo)
+        return res.status(404).send();
+        res.send({todo,code:200});
+    }).catch((e)=>{
+        res.status(400).send();
+    });
 });
 app.listen(port,()=>{
     console.log('Server listening on port:3000');
