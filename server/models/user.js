215d6d3f1c9df59d9115b0a90d7a0bd9dd7d9489
Akshit Sehgal <akshitsehgal95@gmail.com>
2017-12-02 20:12:40 +0530

    Add POST/users route with JWT and Hashing

--- server/models/user.js
+++ server/models/user.js
@@ -1,31 +1,48 @@
 const mongoose=require('mongoose');
 const validator=require('validator');
-var User=mongoose.model('User',{
-   email:{
-       type:String,
-       required:true,
-       trim:true,
-       minLength:1,
-       unique: true,
-       validate:{
-           validator:validator.isEmail,
-           message:'{VALUE} is not a valid email.'
-       }
-   },
-   password:{
-       type:String,
-       require:true,
-       minLength:6
-   },
-   tokens:[{
-       access:{
+const jwt=require('jsonwebtoken');
+const _=require('lodash');
+var UserSchema=new mongoose.Schema({
+    email:{
         type:String,
-        required:true
-       },
-       token:{
+        required:true,
+        trim:true,
+        minLength:1,
+        unique: true,
+        validate:{
+            validator:validator.isEmail,
+            message:'{VALUE} is not a valid email.'
+        }
+    },
+    password:{
         type:String,
-        required:true
-       }
-   }]
-});
+        require:true,
+        minLength:6
+    },
+    tokens:[{
+        access:{
+         type:String,
+         required:true
+        },
+        token:{
+         type:String,
+         required:true
+        }
+    }]
+ });
+UserSchema.methods.toJSON=function(){
+    let user=this;
+    var userObject=user.toObject();
+    return _.pick(userObject,['_id','email']);
+}
+UserSchema.methods.generateAuthToken=function (){
+    var user=this;
+    var access='auth';
+    var token=jwt.sign({_id:user._id.toHexString(),access},'abc123').toString();
+    user.tokens.push({access,token});
+    return user.save().then(()=>{
+        return token;
+    });
+}
+var User=mongoose.model('User',UserSchema);
 module.exports={User};
