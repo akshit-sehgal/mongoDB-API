a4208cef98ffbc007120a25d196e8c3ce07252e5
Akshit Sehgal <akshitsehgal95@gmail.com>
2017-12-03 12:55:55 +0530

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -60,6 +60,20 @@ UserSchema.statics.findByToken=function(token){
 
    })
 }
+UserSchema.statics.findByCredentials=function(email,password){
+    var User=this;
+    return User.findOne({email}).then((user)=>{
+        if(!user) return Promise.reject('No user found');
+        return new Promise((resolve,reject)=>{
+            bcrypt.compare(password,user.password,(err,res)=>{
+                if(res)
+                resolve(user);
+                else
+                reject('Invalid email or password');
+            });
+        });
+    });
+}
 UserSchema.pre('save',function(next){
     let user=this;
     if(user.isModified('password')){
